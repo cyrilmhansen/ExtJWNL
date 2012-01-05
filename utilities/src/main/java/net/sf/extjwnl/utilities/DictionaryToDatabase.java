948ce2d5e40dadbf045d2fb0f820b172109c0c8e
Cyril Michel HANSEN <cyril.hansen@gmail.com>
2012-01-05 13:28:39 +0100

    Use lowercase for table names (linux compatibility)

--- utilities/src/main/java/net/sf/extjwnl/utilities/DictionaryToDatabase.java
+++ utilities/src/main/java/net/sf/extjwnl/utilities/DictionaryToDatabase.java
@@ -30,15 +30,23 @@ public class DictionaryToDatabase {
 
     protected static int INTERNAL_ID = 0;
     protected static long TIME = 0L;
-
+    
+    public static final String INDEXWORD_TABLE = "indexword";
+    public static final String SYNSET_TABLE = "synset";
+    public static final String SYNSETWORD_TABLE = "synsetword";
+    public static final String SYNSETPOINTER_TABLE = "synsetpointer";
+    public static final String SYNSETVERBFRAME_TABLE = "synsetverbframe";
+    public static final String INDEXWORDSYNSET_TABLE = "indexwordsynset";
+    public static final String EXCEPTIONS_TABLE = "exceptions";
+    
     protected static final String[] tables = {
-            "IndexWord",
-            "Synset",
-            "SynsetWord",
-            "SynsetPointer",
-            "SynsetVerbFrame",
-            "IndexWordSynset",
-            "Exceptions"
+    	INDEXWORD_TABLE,
+    	SYNSET_TABLE,
+    	SYNSETWORD_TABLE,
+    	SYNSETPOINTER_TABLE,
+    	SYNSETVERBFRAME_TABLE,
+    	INDEXWORDSYNSET_TABLE,
+    	EXCEPTIONS_TABLE
     };
 
     /**
@@ -207,7 +215,7 @@ public class DictionaryToDatabase {
      */
     protected void storeIndexWords(Iterator<IndexWord> itr) throws SQLException {
         log.info("storing index words");
-        PreparedStatement iwStmt = connection.prepareStatement("INSERT INTO IndexWord VALUES(?,?,?)");
+        PreparedStatement iwStmt = connection.prepareStatement("INSERT INTO "+INDEXWORD_TABLE+" VALUES(?,?,?)");
         int count = 0;
         while (itr.hasNext()) {
             if (count % 1000 == 0) {
@@ -233,10 +241,10 @@ public class DictionaryToDatabase {
      * @throws SQLException SQLException
      */
     protected void storeSynsets(Iterator<Synset> itr) throws SQLException {
-        PreparedStatement synsetStmt = connection.prepareStatement("INSERT INTO Synset VALUES(?,?,?,?,?,?)");
-        PreparedStatement synsetWordStmt = connection.prepareStatement("INSERT INTO SynsetWord VALUES(?,?,?,?,?,?)");
-        PreparedStatement synsetPointerStmt = connection.prepareStatement("INSERT INTO SynsetPointer VALUES(?,?,?,?,?,?,?)");
-        PreparedStatement synsetVerbFrameStmt = connection.prepareStatement("INSERT INTO SynsetVerbFrame VALUES(?,?,?,?)");
+        PreparedStatement synsetStmt = connection.prepareStatement("INSERT INTO "+SYNSET_TABLE+" VALUES(?,?,?,?,?,?)");
+        PreparedStatement synsetWordStmt = connection.prepareStatement("INSERT INTO "+SYNSETWORD_TABLE+" VALUES(?,?,?,?,?,?)");
+        PreparedStatement synsetPointerStmt = connection.prepareStatement("INSERT INTO "+SYNSETPOINTER_TABLE+" VALUES(?,?,?,?,?,?,?)");
+        PreparedStatement synsetVerbFrameStmt = connection.prepareStatement("INSERT INTO "+SYNSETVERBFRAME_TABLE+" VALUES(?,?,?,?)");
         log.info("storing synsets");
         int count = 0;
         while (itr.hasNext()) {
@@ -315,7 +323,7 @@ public class DictionaryToDatabase {
      */
     protected void storeIndexWordSynsets() throws SQLException {
         log.info("storing index word synsets");
-        PreparedStatement iwsStmt = connection.prepareStatement("INSERT INTO IndexWordSynset VALUES(?,?,?)");
+        PreparedStatement iwsStmt = connection.prepareStatement("INSERT INTO "+INDEXWORDSYNSET_TABLE+" VALUES(?,?,?)");
         int count = 0;
         for (Map.Entry<Integer, long[]> entry : idToSynsetOffset.entrySet()) {
             if (count % 1000 == 0) {
@@ -346,7 +354,7 @@ public class DictionaryToDatabase {
      */
     protected void storeExceptions(Iterator<Exc> itr) throws SQLException {
         log.info("storing exceptions");
-        PreparedStatement exStmt = connection.prepareStatement("INSERT INTO Exceptions VALUES(?,?,?,?)");
+        PreparedStatement exStmt = connection.prepareStatement("INSERT INTO "+EXCEPTIONS_TABLE+" VALUES(?,?,?,?)");
         while (itr.hasNext()) {
             Exc exc = itr.next();
             exStmt.setString(4, exc.getLemma());
